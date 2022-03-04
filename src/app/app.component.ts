7c61a8ae509812230522dd055be3a3ff61a51c15
Fran <vidotto982@gmail.com>
2022-03-04 20:34:42 -0300

    add styles in profile and menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
-
+import { Component, AfterViewInit  } from '@angular/core';
+declare var anime: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'portafolio';
+  ngAfterViewInit(): void {
+}
 }
--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,16 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, AfterViewInit  } from '@angular/core';
+declare var anime: any;
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.css']
 })
-export class InicioComponent implements OnInit {
+export class InicioComponent implements OnInit, AfterViewInit{
   showFiller = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  ngAfterViewInit(): void {
+    let textWrapper = document.querySelector('.name .letters');
+    let textWrapperSecond = document.querySelector('.title .letters');
+    // @ts-ignore
+    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter' style='display:inline-block;'>$&</span>");
+    // @ts-ignore
+    textWrapperSecond.innerHTML = textWrapperSecond.textContent.replace(/\S/g, "<span class='letter' style='display:inline-block;'>$&</span>");
 
+    anime.timeline({loop: true})
+      .add({
+        targets: '.name , .letter , .title',
+        rotateY: [-90, 0],
+        duration: 2300,
+        delay: (el: any, i: number) => 85 * i
+      }).add({
+      targets: '.name , .title',
+      opacity: 0,
+      duration: 2000,
+      easing: "easeOutExpo",
+      delay: 1000
+    });
+  }
 }
